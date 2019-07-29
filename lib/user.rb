087b5e7d00bd38addef72ca56376dda282070720
Kevin Kamanda <kevkamanda@gmail.com>
2019-07-29 20:26:13 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+  
+   attr_accessor :first_name, :last_name
+   attr_reader :knowledge
+  
+  def initialize()
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+  
+  def teach
+    knowledge.each do |s|
+      
+  
+end
